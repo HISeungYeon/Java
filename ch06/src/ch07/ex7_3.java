e927ac6ec92db192d06182c7a745da439539a0e6
HISeungYeon <sy.lee5081@gmail.com>
2023-06-20 22:23:58 +0900

    ex7

--- ch06/src/ch07/ex7_3.java
+++ ch06/src/ch07/ex7_3.java
@@ -2,29 +2,29 @@ package ch07;
 
 public class ex7_3 {
 	public static void main(String[] args) {
-		Buyer b = new Buyer();
+		Buyer3 b = new Buyer3();
 		
-		b.buy(new Tv2()); // buy(Product p)
-		b.buy(new Computer()); //buy(Product p)
+		b.buy(new Tv3()); // buy(Product33 p)
+		b.buy(new Computer3()); //buy(Product33 p)
 		
 		System.out.println("현재 남은 돈은 " + b.money + "만원 입니다.");
 		System.out.println("현재 보너스점수는 " + b.bonumsPoint + "점입니다.");
 	}
 }
 
-class Product{
+class Product3{
 	int price; // 제품의 가격
 	int bonusPoint; //제품 구매 시 제공하는 보너스 점수
 	
-	Product (int price){
+	Product3 (int price){
 		this.price = price;
 		bonusPoint = (int)(price/10.0); // 보너스 점수는 제품가격의 10%
 	}
 }
 
-class Tv2 extends Product{
-	Tv2(){
-		// 조상클래스의 생성자 Product (int price)를 호출한다.
+class Tv3 extends Product3{
+	Tv3(){
+		// 조상클래스의 생성자 Product33 (int price)를 호출한다.
 		super(100); // Tv의 가격을 100만원으로 한다.
 	}
 	
@@ -34,21 +34,21 @@ class Tv2 extends Product{
 	}
 }
 
-class Computer extends Product {
-	Computer() {
+class Computer3 extends Product3 {
+	Computer3() {
 		super(200);
 	}
 	
 	public String toString() {
-		return "Computer";
+		return "Computer3";
 	}
 }
 
-class Buyer{ // 고객, 물건을 사는 사람
+class Buyer3{ // 고객, 물건을 사는 사람
 	int money = 1000; // 소유 금액
 	int bonumsPoint = 0; //보너스 점수
 	
-	void buy(Product p) {
+	void buy(Product3 p) {
 		if(money < p.price) {
 			System.out.println("잔액이 부족하여 물건을 살 수 없습니다");
 			return;
